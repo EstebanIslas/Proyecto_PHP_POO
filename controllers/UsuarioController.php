5e38b233604f1827a7b55fd6fb8b828b18335c9a
Esteban Islas <1717110261@utectulancingo.edu.mx>
2020-06-11 15:29:57 -0500

    Formulario de registro mvc

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -7,4 +7,16 @@ class usuarioController{
     {
         echo 'Controlador Usuario Accion index';
     }
+    #Metodo para ingresar al formulario
+    public function registro()
+    {
+        require_once 'views/usuario/registro.php';
+    }
+    public function save()
+    {
+        #Valida si existen los valores
+        if (isset($_POST)) {
+            var_dump($_POST);
+        }
+    }
 }
