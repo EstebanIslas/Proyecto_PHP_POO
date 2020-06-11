a50d9e87adf61dfbf5e0bcbffb999e63fd6b9d02
Esteban Islas <1717110261@utectulancingo.edu.mx>
2020-06-11 13:03:33 -0500

    Maquetado de Productos

--- index.php
+++ index.php
@@ -80,21 +80,21 @@
                         <img src="assets/img/camiseta.png">
                         <h2>Camiseta Azul Hancha</h2>
                         <p>30 euros</p>
-                        <a href="">Comprar</a>
+                        <a href="" class="button">Comprar</a>
                     </div>
 
                     <div class="product">
                         <img src="assets/img/camiseta.png">
                         <h2>Camiseta Azul Hancha</h2>
                         <p>30 euros</p>
-                        <a href="">Comprar</a>
+                        <a href="" class="button">Comprar</a>
                     </div>
 
                     <div class="product">
                         <img src="assets/img/camiseta.png">
                         <h2>Camiseta Azul Hancha</h2>
                         <p>30 euros</p>
-                        <a href="">Comprar</a>
+                        <a href="" class="button">Comprar</a>
                     </div>
                 </div>
             </div>
